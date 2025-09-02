4917f3dc4f277b809f029da66fbb50e602132883
Shiv Chaurasiya <shivchaurasiya530@gmail.com>
2025-09-02 13:03:19 +0530

    Added .html .css .js files

---
+++ index.js
@@ -0,0 +1,35 @@
+
+let myStr=""
+
+const populate = async (value, currency) => {
+    url =
+        "https://api.currencyapi.com/v3/latest?apikey=cur_live_xPzoJKaCLBrjOrE1lp5mVaKjnNHEZOQ8xNhCCWa9&base_currency=" +
+        currency;
+
+        let response = await fetch(url);
+        let rJson = await response.json();
+        console.log(rJson);
+
+        for (let key of Object.keys(rJson["data"])) {
+            console.log(key);
+            myStr +=    `<tr>
+                        <td>${key}</td>
+                        <td>${rJson["data"][key]["code"]}</td>
+                        <td>${Math.round(rJson["data"][key]["value"] * value )}</td>
+                        </tr>
+                        `
+        }
+
+    const tableBody = document.querySelector("tbody");
+    tableBody.innerHTML = myStr;
+};
+
+const btn = document.querySelector(".btn");
+
+btn.addEventListener("click", (e) => {
+    e.preventDefault();
+    const value = parseInt(document.querySelector("input[name ='quantity']").value);
+    const currency = document.querySelector("select[name ='currency']").value;
+    populate(value, currency);
+    document.querySelector(".output").classList.remove("output");
+});
